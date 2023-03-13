1a58adc0f75b1954377805f850d94f84c1c7708f
emirhan <emir_58851@outlook.com>
2023-03-13 15:27:51 +0300

    random

---
+++ 05-random şifer/app.js
@@ -0,0 +1,46 @@
+const getRandomLover = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+}
+const getRandomUpper = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+}
+const getRandomNumber = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+}
+const getRandomSymbol = () => {
+    const symbols = '!@#$%&*'
+    return symbols[Math.floor(Math.random() * symbols.length)]
+}
+
+const randomFunctions = { getRandomLover, getRandomUpper, getRandomNumber, getRandomSymbol }
+
+const generatePassword = () => {
+    const length = 16
+    let generatePassword = ''
+    for (let x = 0; x < length; x++)
+        generatePassword += Object.values(randomFunctions)[Math.floor(Math.random() * 4)]()
+
+    return generatePassword
+}
+
+const spanResult = document.getElementById('result')
+const generateButton = document.getElementById('generate')
+const clipboardButton = document.getElementById('clipboard')
+
+generateButton.addEventListener('click', () => {
+    spanResult.innerText = generatePassword()
+})
+
+clipboardButton.addEventListener('click', () => {
+    const password = spanResult.innerText
+    if (!password)
+        return
+    const textarea = document.createElement('textarea')
+    textarea.value = password
+    document.body.appendChild(textarea)
+    textarea.select()
+    document.execCommand('copy')
+    textarea.remove()
+    console.log('parola koyalandı...')
+})
+
